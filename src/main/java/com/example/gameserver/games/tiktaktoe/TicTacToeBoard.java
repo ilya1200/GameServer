919972619a54e61d04c85ab2e000d381f55f0318
ilya1200 <ilya1200@gmail.com>
2023-06-03 11:52:20 +0300

    Add TicTacToeBoard

---
+++ src/main/java/com/example/gameserver/games/tiktaktoe/TicTacToeBoard.java
@@ -0,0 +1,137 @@
+package com.example.gameserver.games.tiktaktoe;
+import com.example.gameserver.games.Board;
+import com.example.gameserver.games.Player;
+
+public class TicTacToeBoard implements Board{
+    private static final int BOARD_SIZE = 3;
+
+    private final Player[][] cell;
+    private Player currentPlayer;
+
+    private boolean isGameFinished;
+
+    public TicTacToeBoard(){
+        cell = new Player[BOARD_SIZE][BOARD_SIZE];
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                cell[row][col] = null;
+            }
+        }
+        currentPlayer = Player.FIRST;
+        isGameFinished = false;
+    }
+
+
+    private void switchPlayer() {
+        if (currentPlayer == Player.FIRST) {
+            currentPlayer = Player.SECOND;
+        } else {
+            currentPlayer = Player.FIRST;
+        }
+    }
+
+    @Override
+    public void makeMove(String move, Player player) {
+        int row =0 ;
+        int col = 0;
+        // parsing
+        this.makeMove(player, row, col);
+
+
+
+        if(isWin()){
+            isGameFinished = true;
+            System.out.println("Player " + (currentPlayer == Player.FIRST ? Player.FIRST : Player.SECOND) + " won!");
+        } else if (isDraw()) {
+            isGameFinished = true;
+            System.out.println("Draw!");
+        }else{
+            switchPlayer();
+        }
+    }
+
+    private void makeMove(Player player, int row, int col) throws IllegalArgumentException {
+        if(!isValidMove(row,col)){
+            throw new IllegalArgumentException("Illegal move");
+        }
+        cell[row][col] = player;
+    }
+
+    public boolean isValidMove(int row, int column) {
+        boolean is_move_in_bound = (row >= 0 && row < BOARD_SIZE && column >= 0 && column < BOARD_SIZE);
+        return is_move_in_bound && cell[row][column] == null;
+    }
+
+    public boolean isWin() {
+        // Check rows and columns for a win
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            boolean rowWin = true;
+            boolean colWin = true;
+
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                // Check if any element in the row is different from the first element or is empty
+                if (cell[i][j] != cell[i][0] || cell[i][0] == null) {
+                    rowWin = false;
+                }
+
+                // Check if any element in the column is different from the first element or is empty
+                if (cell[j][i] != cell[0][i] || cell[0][i] == null) {
+                    colWin = false;
+                }
+            }
+
+            // If a row or column has all elements equal and not empty, return true (win condition met)
+            if (rowWin || colWin) {
+                return true;
+            }
+        }
+
+        boolean diagonalWin = true;
+        boolean antiDiagonalWin = true;
+
+        // Check diagonals for a win
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            // Check if any element in the main diagonal is different from the first element or is empty
+            if (cell[i][i] != cell[0][0] || cell[0][0] == null) {
+                diagonalWin = false;
+            }
+
+            // Check if any element in the anti-diagonal is different from the first element or is empty
+            if (cell[i][BOARD_SIZE - 1 - i] != cell[0][BOARD_SIZE - 1] || cell[0][BOARD_SIZE - 1] == null) {
+                antiDiagonalWin = false;
+            }
+        }
+
+        // If either the diagonal or anti-diagonal has all elements equal and not empty, return true (win condition met)
+        return diagonalWin || antiDiagonalWin;
+    }
+
+    public boolean isDraw() {
+        if (isWin()) {
+            return false; // If there is a win, it's not a draw
+        }
+
+        // Check if any empty cell exists
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                if (cell[i][j] == null) {
+                    return false; // If empty cell found, it's not a draw
+                }
+            }
+        }
+
+        // All cells are filled, and there is no win, it's a draw
+        return true;
+    }
+
+    public void displayBoard() {
+        for (Player[] players : cell) {
+            for (Player aPlayer : players) {
+                char aChar = aPlayer == null? ' ':aPlayer==Player.FIRST?'X':'O';
+                System.out.print(" | " + aChar + " | ");
+            }
+            System.out.println();
+        }
+    }
+
+}
